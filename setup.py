5bc23af0956f75cb76f967fcb4014d3a294189fb
joe-habel <=>
2018-10-13 06:47:51 -0400

    Fixed setup file

--- setup.py
+++ setup.py
@@ -1,4 +1,29 @@
-from distutils.core import setup
-import py2exe
+from cx_Freeze import setup, Executable
+# NOTE: you can include any other necessary external imports here aswell
+ 
+includefiles = ['config.txt','readme.md'] # include any files here that you wish
+includes = []
+excludes = []
+packages = []
+ 
+chrome = Executable(
+ # what to build
+   script = "update_chrome_files.py", # the name of your main python script goes here 
+   targetName = "update_chrome_Files.exe", # this is the name of the executable file
+)
 
-setup(console=['update_chrome_files.py'])
+installer = Executable(
+ # what to build
+   script = "installer.py", # the name of your main python script goes here 
+   targetName = "Setup.exe", # this is the name of the executable file
+)
+ 
+setup(
+ # the actual setup & the definition of other misc. info
+    name = "Global Chrome Fixer", # program name
+    version = "0.1",
+    author = "Joe Habel",
+    options = {"build_exe": {"excludes":excludes,"packages":packages,
+      "include_files":includefiles}},
+    executables = [chrome,installer]
+)
